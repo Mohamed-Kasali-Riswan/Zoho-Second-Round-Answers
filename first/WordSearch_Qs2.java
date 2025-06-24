8189ac7d6dd23704339a2ac55d1d00af86707e1c
Mohamed-Kasali-Riswan <146856255+Mohamed-Kasali-Riswan@users.noreply.github.com>
2025-06-24 15:43:51 +0530

    Update WordSearch_Qs2.java

--- first/WordSearch_Qs2.java
+++ first/WordSearch_Qs2.java
@@ -4,8 +4,23 @@ public class WordSearch_Qs2 {
 
 	public static void main(String[] args) {
 		
+		String[] arr = { "i", "like", "sam", "sung", "samsung", "mobile", "ice","cream", "icecream", "man", "go", "mango"};
+				
+		String s="ilike";
 		
+		if(backTrack(arr,s)) System.out.println("YEs");
+		else System.out.println("No");
+	}
 
+	private static boolean backTrack(String[] arr, String s) {
+		
+		if(s.length()==0 || s==null) return true;
+		
+		for(String k:arr) {
+			if(s.startsWith(k)) return backTrack(arr, s.substring(k.length()));
+		}
+		
+		return false;
 	}
 
 }
